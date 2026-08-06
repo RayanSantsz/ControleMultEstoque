563c27dae89b7ecb89ab806914ebceb8ced7b2dd
Rayan Santos <rayansantos0088@gmail.com>
2026-08-06 11:05:15 -0300

    Interface repositorio armazem

--- ControleMultEstoque.Domain/Interfaces/IArmazemRepository.cs
+++ ControleMultEstoque.Domain/Interfaces/IArmazemRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ControleMultEstoque.Domain.Entities;
 
 namespace ControleMultEstoque.Domain.Interfaces
 {
-    internal interface IArmazemRepository
+    public interface IArmazemRepository : IRepositorioBase<Armazem>
     {
+        Task<Armazem?> ObterPorResponsavelAsync(Guid responsavelId);
     }
 }
