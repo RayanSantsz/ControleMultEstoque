c7a2c816de1ab4ef27bc458f1ade47d7df2d6e96
Rayan Santos <rayansantos0088@gmail.com>
2026-08-06 11:21:08 -0300

    Interface base para repositorios

--- ControleMultEstoque.Domain/Interfaces/IRepositorioBase.cs
+++ ControleMultEstoque.Domain/Interfaces/IRepositorioBase.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace ControleMultEstoque.Domain.Interfaces
 {
-    internal interface IRepositorioBase
+    public interface IRepositorioBase<TEntidade> where TEntidade : class
     {
+        Task<TEntidade?> ObterPorIdAsync(Guid id);
+        Task<IReadOnlyList<TEntidade>> ObterTodosAsync();
+        Task AdicionarAsync(TEntidade entidade);
+        void Remover(TEntidade entidade);
     }
 }
